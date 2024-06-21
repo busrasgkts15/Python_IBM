ba006598fe38e48616420c62a06ab9a7eb1c2d15
Büşra Göktaş <busra1gkts@gmail.com>
2024-06-21 21:33:42 +0300

    Yas Hesabi

--- hello.py
+++ hello.py
@@ -1,2 +1,12 @@
-x = 2
-print(x)
+old = int(input("Yaşınızı giriniz : "))
+if old < 18:
+    print("Yaşınız küçük")
+    
+elif old == 18:
+    print("Yaşınız 18 ve uygun")
+    
+else:
+    print("Yaşınız yeterli :)")
+    
+    
+print("Program bitti.")
