0c5a4d4c0cb34114e366df349a39ad2400fb81f0
Büşra Göktaş <busra1gkts@gmail.com>
2024-06-23 16:32:38 +0300

    try

--- hello.py
+++ hello.py
@@ -10,3 +10,15 @@ else:
     
     
 print("Program bitti.")
+old1 = int(input("Yaşınızı giriniz : "))
+if old < 18:
+    print("Yaşınız küçük")
+    
+elif old == 18:
+    print("Yaşınız 18 ve uygun")
+    
+else:
+    print("Yaşınız yeterli :)")
+    
+    
+print("Program bitti.")
